c7539a6f01afd8e5408bb5d59bfeefe643db2250
aldyansah77 <aldyansahsmk01@gmail.com>
2024-06-06 20:51:35 +0700

    Create day2.py

---
+++ day 2/day2.py
@@ -0,0 +1,13 @@
+#mengambil input nama
+nama = input('siapa nama kamu : ') 
+
+#mengambil inputan umur
+umur = input('berapa umur kamu : ')
+
+#mengambil inputan universitas
+universitas = input('nama universitasmu : ')
+
+#menampilkan variabel
+print('hallo' , nama) 
+print('umur kamu' , umur) 
+print('menempuh pendidikan di' , universitas)
