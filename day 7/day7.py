41a1e8f2125c75b66e47e467b4681af291aeb1d0
aldyansah77 <aldyansahsmk01@gmail.com>
2024-06-11 23:16:29 +0700

    Update day7.py

--- day 7/day7.py
+++ day 7/day7.py
@@ -1,25 +1,10 @@
-# Fungsi untuk mengecek apakah suatu bilangan prima
-def is_prime(num):
-    if num <= 1:
-        return False
-    for i in range(2, num):
-        if num % i == 0:
-            return False
-    return True
+# Meminta pengguna untuk memasukkan sebuah bilangan
+bilangan = int(input("Masukkan sebuah bilangan: "))
 
-# Meminta pengguna untuk memasukkan nilai N
-N = int(input("Masukkan nilai N: "))
+# Menampilkan faktor dari bilangan yang dimasukkan
+print("Faktor dari", bilangan, "adalah:")
 
-print("Deret bilangan prima:")
-# Perulangan for untuk mencetak deret bilangan prima
-for i in range(2, N+1):
-    if is_prime(i):
-        print(i, end=" ")
-
-print("\nDeret bilangan prima (menggunakan while):")
-# Perulangan while untuk mencetak deret bilangan prima
-num = 2
-while num <= N:
-    if is_prime(num):
-        print(num, end=" ")
-    num += 1
+# Menggunakan perulangan for untuk mencari faktor bilangan
+for i in range(1, bilangan + 1):
+    if bilangan % i == 0:
+        print(i)
