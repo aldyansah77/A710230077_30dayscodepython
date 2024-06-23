6939214dc10e1fa034d419ec3660327c3db06486
aldyansah77 <aldyansahsmk01@gmail.com>
2024-06-24 02:32:02 +0700

    Create day3.py

---
+++ day3coding30day/day3.py
@@ -0,0 +1,9 @@
+# Data dalam variabel
+jenis_buku = "novel" 
+jumlah_buku = 175
+harga_buku = 15000.50
+
+# Mengecek tipe data dalam variabel
+print(f'Tipe data dari data {jenis_buku} adalah {type(jenis_buku)}') # tipe data string (str)
+print(f'Tipe data dari data {jumlah_buku} adalah {type(jumlah_buku)}') # tipe data integer (int)
+print(f'Tipe data dari data {harga_buku} adalah {type(harga_buku)}') # tipe data float
